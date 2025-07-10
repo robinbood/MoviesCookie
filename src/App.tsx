99b1feea6e8975628fb85073d6daef96af1fa32b
robinbood <mercedesfarts@gmail.com>
2025-07-10 15:57:05 +0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+// import "./index.css";
+import {SplashPage} from "./SplashPage";
+
+
+function App() {
+  const [splash,setSplash] = useState<boolean>(true)
+
+  return (
+    <div className="app">
+
+     {splash && <SplashPage/>}
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ src/SplashPage.tsx
@@ -0,0 +1,36 @@
+import "./Splash.css"
+export const SplashPage = () => {
+    return (
+      <div className="Splash">
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+
+
+        <div className="logo">
+
+        </div>
+
+        <h2>Welcome to the experience</h2>
+
+
+        <div className="loading-container">
+          <div className="loading-bar"></div>
+        </div>
+
+      </div>
+    )
+}
+
