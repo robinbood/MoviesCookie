11d333b11e17335ee8b957f03496980ad6977aaa
robinbood <mercedesfarts@gmail.com>
2025-08-01 14:55:43 +0500

    initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ const server = serve({
 
     "/api/login": {
       
-      async PUT(req) {
+      async POST(req) {
         return Response.json({
           message: "Hello, world!",
           method: "PUT",
