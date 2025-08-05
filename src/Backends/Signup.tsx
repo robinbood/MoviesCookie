3720db4e71d4b0f5f3341967357d2bbf76d99692
robinbood <mercedesfarts@gmail.com>
2025-08-05 12:09:45 +0500

    initial commit

--- src/Backends/Signup.tsx
+++ src/Backends/Signup.tsx
@@ -1,11 +1,28 @@
 import { SQL } from "bun";
+import { eq } from "drizzle-orm";
 import { drizzle } from "drizzle-orm/bun-sql";
-import { users } from "src/db/schema"; 
+import { users } from "src/db/schema";
 
 const client = new SQL(Bun.env.DATABASE_URL!);
 const db = drizzle(client);
 
-const SigninUser = async (req: Request) => {
-    const { name, username, password } = await req.json();
-    const exists = await db.select().from(users);
-}
+const SignUpUser = async (req: Request) => {
+  const { name, username, password } = await req.json();
+  const exists = await db
+    .select()
+    .from(users)
+    .where(eq(users.username, username));
+  if (exists) {
+    return new Response("Username already exists", { status: 400 });
+  }
+
+  const newPassword = await Bun.password.hash(password, {
+    algorithm: "argon2id",
+    memoryCost: 4,
+    timeCost: 3,
+  });
+  await db.insert(users).values({ username, password: newPassword });
+  return new Response("User created successfully", { status: 201 });
+};
+
+export default SignUpUser;
